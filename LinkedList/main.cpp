0bef2e1be668f8200cf290b8da8bac021519a26e
santosh-softy-lover <santoshmohanramk1383@gmail.com>
2020-09-12 23:26:46 +0530

    #define tutorial-1

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+using namespace std;
 #define name(santosh) santosh > 1 ? 1:0
 #define n 10
 
-#ifdef n
-    printf("name is defined \n");
-#endif
 
 
 int main(){
+    #ifdef n
+        cout << ("name is defined \n");
+    #endif
+
     bool x = name(10);
     std::cout << x << std::endl;
     return 0;
