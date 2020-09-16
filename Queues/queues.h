830456e81a6e8c1537b363f6a02c0d33a479be69
santosh-softy-lover <santoshmohanramk1383@gmail.com>
2020-09-16 15:35:59 +0530

    Queues phase -1

--- Queues/queues.h
+++ Queues/queues.h
@@ -3,11 +3,11 @@ template<class t>
 class queue{
     private:
         int capacity,front,rear;
-        int *Q;
+        t *Q;
     public:
         void init(int size);
         void Enqueue(t data);
-        l Dequeue(void);
+        t Dequeue(void);
         int length(void);
         bool isEmpty(void);
 };
@@ -15,9 +15,27 @@ class queue{
 
 template<class t>
 void queue<t>::init(int size){
-    
+    capacity = size;
+    front = rear = -1;
+    Q = (t *)malloc(sizeof(t) * capacity);
 }
 
+template<class t>
+void queue<t>::Enqueue(t data){
+    rear = (rear + 1) % capacity;
+    Q[rear] = data;
+    if(front = -1) front = rear;
+}
 
+template<class t>
+t queue<t>::Dequeue(void){
+    if(isEmpty()) std::cout << "The Queue is Empty " << std::endl;
+    else{
+        t deleted = Q[front];
+        if(front == rear) front == rear = -1;
+        else front = (front + 1) % capacity;
+        return t;
+    }
+}
 
 
