de930d83e5217433db9234c99cea544afcc59489
santosh-softy-lover <santoshmohanramk1383@gmail.com>
2020-09-16 15:15:29 +0530

    Queues folder create

---
+++ Queues/queues.h
@@ -0,0 +1,23 @@
+
+template<class t>
+class queue{
+    private:
+        int capacity,front,rear;
+        int *Q;
+    public:
+        void init(int size);
+        void Enqueue(t data);
+        l Dequeue(void);
+        int length(void);
+        bool isEmpty(void);
+};
+
+
+template<class t>
+void queue<t>::init(int size){
+    
+}
+
+
+
+
