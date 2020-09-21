bb316225e109088dd891070333eae5fabeaeba74
santosh-softy-lover <santoshmohanramk1383@gmail.com>
2020-09-21 12:35:26 +0530

    traversal recursive methods

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -4,17 +4,22 @@
 int main(){
 
     Tree<int> tree;
-    tree.root->data = 30;
+    tree.root->data = 10;
     
     tree.root->left = new node<int>;
-    tree.root->left->data = 10;
+    tree.root->left->data = 20;
+    tree.root->left->left = NULL;
+    tree.root->left->right = NULL;
     
     tree.root->right = new node<int>;
-    tree.root->right->data = 20;
-    
-    std::cout << tree.root->data << std::endl;
-    
-    //tree.preordertraversal(tree.root);
+    tree.root->right->data = 30;
+    tree.root->right->left = NULL;
+    tree.root->right->right = NULL;
 
+    tree.preordertraversal(tree.root);
+    std::cout << "------------------------------------------------ \n";
+    tree.inordertraversal(tree.root);
+    std::cout << "------------------------------------------------ \n";
+    tree.postodertraversal(tree.root);
     return 0;
 }
--- Tree/tree.h
+++ Tree/tree.h
@@ -15,14 +15,40 @@ class Tree{
     public:
         struct node<t> *root;
         void preordertraversal(struct node<t> *root);
+        void postodertraversal(struct node<t> *root);
+        void inordertraversal(struct node<t> *root);
+        Tree(){
+            root = new node<t>;
+            root->data = 0;
+            root->left = NULL;
+            root->right = NULL;
+        }
 }; 
 
 template<class t>
 void Tree<t>::preordertraversal(struct node<t> *root){
-    while(root){
-        std::cout << root->data;
+    if(root){
+        std::cout << root->data << std::endl;
         preordertraversal(root->left);
         preordertraversal(root->right); 
+    }
+}
+
+template<class t>
+void Tree<t>::postodertraversal(struct node<t> *root){
+    if(root){
+        postodertraversal(root->left);
+        postodertraversal(root->right);
+        std::cout << root->data << std::endl;
+    }
+}
+
+template<class t>
+void Tree<t>::inordertraversal(struct node<t> *root){
+    if(root){
+        postodertraversal(root->left);
+        std::cout << root->data << std::endl;
+        postodertraversal(root->right);
     }
 }
 #endif
